3bbc1dc015ec444c97303c402f42192ba62c1d32
Wesley Camargo <wesley.camargo@TCSNLPS.onmicrosoft.com>
2025-05-12 15:34:19 +0200

    Updating publish script

--- src/helper-functions/Publish-ToAzureArtifactsPSRepo.ps1
+++ src/helper-functions/Publish-ToAzureArtifactsPSRepo.ps1
@@ -64,36 +64,31 @@ function Publish-ToAzureArtifactsPSRepo {
         [Parameter(Mandatory)]
         [string]$ApiKey
     )
-
+    
     try {
         $SecretVault = "LocalVault"
-
-        Write-Host "Installing and importing required modules..." -ForegroundColor Cyan
-        Install-Module -Name Microsoft.PowerShell.SecretStore -Repository PSGallery -Force -ErrorAction Stop
-        Install-Module -Name Microsoft.PowerShell.SecretManagement -Repository PSGallery -Force -ErrorAction Stop
-        Install-Module -Name Microsoft.PowerShell.PSResourceGet -Repository PSGallery -Force -ErrorAction Stop
-        Import-Module Microsoft.PowerShell.SecretStore
-        Import-Module Microsoft.PowerShell.SecretManagement
-        Import-Module Microsoft.PowerShell.PSResourceGet -RequiredVersion 1.1.1
-
-        $feedUrl = "https://pkgs.dev.azure.com/$Organization/$Project/_packaging/$FeedName/nuget/v2"
+    
+        $feedUrl = "https://pkgs.dev.azure.com/$Organization/$Project/_packaging/$FeedName/nuget/v3/index.json"
         $secureToken = $PatToken | ConvertTo-SecureString -AsPlainText -Force
         $credential = [PSCredential]::new($Username, $secureToken)
-
+    
         if (-not (Get-SecretVault -Name $SecretVault -ErrorAction SilentlyContinue)) {
             Register-SecretVault -Name $SecretVault -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
         }
-
+    
         Reset-SecretStore -Authentication None -Interaction None -Force
         Set-Secret -Name "MyCredential" -Secret $credential -Vault $SecretVault
-
-        if (-not (Get-PSRepository -Name $RepositoryName -ErrorAction SilentlyContinue)) {
-            Register-PSRepository -Name $RepositoryName -SourceLocation $feedUrl -InstallationPolicy Trusted -Credential $credential
-        }
-
+    
+        #TODO: Add PSResourceRepository and check if needed Register-PSRepository us still needed
+        if (-not (Get-PSResourceRepository -Name $FeedName -ErrorAction SilentlyContinue)) {
+            Register-PSResourceRepository -Name $FeedName -Uri $feedUrl          
+        }       
+    
         Write-Host "Publishing package from: $PackagePath" -ForegroundColor Cyan
         Publish-PSResource -Path $PackagePath -Repository $FeedName -Credential $credential -ApiKey $ApiKey
-    } catch {
+    }
+    catch {
         Write-Error "❌ Error occurred: $_"
     }
 }
+    
